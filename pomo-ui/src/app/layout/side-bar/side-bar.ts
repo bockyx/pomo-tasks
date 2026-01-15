9aa47a789e37e8b2d99e550d9c9e0a7ea2deeec8
bockyx <bockyx@gmail.com>
2026-01-15 16:12:54 -0400

    feat(side-bar): implement side bar component with expandable menu

--- pomo-ui/src/app/layout/side-bar/side-bar.ts
+++ pomo-ui/src/app/layout/side-bar/side-bar.ts
@@ -1,11 +1,18 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'pou-side-bar',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, NgClass],
   templateUrl: './side-bar.html',
   styleUrl: './side-bar.scss',
 })
 export class SideBar {
+  isExpanded = false;
 
+  toggleExpand() {
+    this.isExpanded = !this.isExpanded;
+  }
 }
