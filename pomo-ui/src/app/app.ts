bddcaf833119b866d99c361f483054790fbb8027
bockyx <bockyx@gmail.com>
2026-01-14 22:46:21 -0400

    feat(app): defined areas for the app

--- pomo-ui/src/app/app.ts
+++ pomo-ui/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TitleBar } from "./layout/title-bar/title-bar";
+import { TitleBar } from './layout/title-bar/title-bar';
+import { SideBar } from '@pomo-ui/layout/side-bar/side-bar';
 
 @Component({
   selector: 'pou-root',
-  imports: [RouterOutlet, TitleBar],
+  imports: [RouterOutlet, TitleBar, SideBar],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('pomo-ui');
