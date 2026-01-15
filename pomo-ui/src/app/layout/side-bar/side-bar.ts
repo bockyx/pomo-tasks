97372d9f26a804c80de2228515e6f9eeb28096b3
bockyx <bockyx@gmail.com>
2026-01-14 22:47:58 -0400

    feat(side-bar): added side bar component to main layout of the app

---
+++ pomo-ui/src/app/layout/side-bar/side-bar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pou-side-bar',
+  imports: [],
+  templateUrl: './side-bar.html',
+  styleUrl: './side-bar.scss',
+})
+export class SideBar {
+
+}
