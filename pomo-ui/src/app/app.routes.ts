0f0785942636eb28e56cf385a8f3eb6e88604213
bockyx <bockyx@gmail.com>
2026-01-19 18:14:44 -0400

    feat(settings): :sparkles: added settings feature

--- pomo-ui/src/app/app.routes.ts
+++ pomo-ui/src/app/app.routes.ts
@@ -9,4 +9,8 @@ export const routes: Routes = [
     path: 'tasks',
     loadComponent: () => import('./features/tasks/tasks').then((m) => m.Tasks),
   },
+  {
+    path: 'settings',
+    loadComponent: () => import('./features/settings/settings').then((m) => m.Settings),
+  },
 ];
